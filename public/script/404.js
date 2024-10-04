354d21be08a5db3feddeafb89eb46dd3e4b35833
Abel <148551591+abelramos01@users.noreply.github.com>
2024-10-04 08:50:06 -0300

    404.js

--- public/script/404.js
+++ public/script/404.js
@@ -84,4 +84,7 @@ const nav = document.querySelector('nav');
 burger.addEventListener('click', e => {
   burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed";
   nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed";
+
+function redirecionar(){
+    window.location.href='index.html';
 });
